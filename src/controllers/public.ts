6d9793489bb5047f894648c80d25c4776f7684ed
backmeupplz <backmeupplz@gmail.com>
2021-04-06 08:47:28 -0700

    add type to context

--- src/controllers/public.ts
+++ src/controllers/public.ts
@@ -1,10 +1,11 @@
 import { EventModel } from '@/models/event'
+import { Context } from 'koa'
 import { Body, Controller, Ctx, Post } from 'koa-ts-controllers'
 
 @Controller('/')
 export default class PublicController {
   @Post('/')
-  async event(@Ctx() ctx, @Body() body) {
+  async event(@Ctx() ctx: Context, @Body() body) {
     try {
       await new EventModel(body).save()
     } catch (err) {
