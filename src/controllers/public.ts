0ebdd86cbc785ef60be1f3ffdf32354b19a9d188
backmeupplz <backmeupplz@gmail.com>
2021-04-06 08:44:14 -0700

    change error

--- src/controllers/public.ts
+++ src/controllers/public.ts
@@ -1,10 +1,14 @@
 import { EventModel } from '@/models/event'
-import { Body, Controller, Post } from 'koa-ts-controllers'
+import { Body, Controller, Ctx, Post } from 'koa-ts-controllers'
 
 @Controller('/')
 export default class PublicController {
   @Post('/')
-  async event(@Body() body: any) {
-    await new EventModel(body).save()
+  async event(@Ctx() ctx, @Body() body) {
+    try {
+      await new EventModel(body).save()
+    } catch (err) {
+      return ctx.throw(400, err)
+    }
   }
 }
